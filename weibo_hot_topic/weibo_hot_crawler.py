c7d6347a057eb8c4d8450c6b2d20e7ef48ad27c6
Xiaochi (George) Li <343928729@qq.com>
2018-12-28 16:35:57 -0500

    add stability

--- weibo_hot_topic/weibo_hot_crawler.py
+++ weibo_hot_topic/weibo_hot_crawler.py
@@ -1,7 +1,6 @@
 from urllib.request import urlopen
 from urllib.parse import quote
 from bs4 import BeautifulSoup
-import pandas as pd
 import time
 from datetime import timedelta, datetime
 import csv
@@ -20,13 +19,16 @@ def transform_number(s):
 def get_read_discuss(keyword):
     link = quote('#'+keyword+'#')
     url = 'https://s.weibo.com/weibo?q='+link
-    response = urlopen(url,timeout=10)
-    soup = BeautifulSoup(response, 'lxml')
+    try:
+        response = urlopen(url, timeout=30)
+        soup = BeautifulSoup(response, 'lxml')
+        text = soup.find('div', 'total')
+        read = transform_number(text.find_all('span')[0].string[2:])
+        discuss = transform_number(text.find_all('span')[1].string[2:])
+        return read, discuss
 
-    text = soup.find('div', 'total')
-    read = transform_number(text.find_all('span')[0].string[2:])
-    discuss = transform_number(text.find_all('span')[1].string[2:])
-    return read,discuss
+    except:
+        return -1, -1
 
 
 def get_time():
@@ -46,20 +48,30 @@ def get_time():
 def get_hot():
     year, month, day, hour, minute = get_time()
     hot_url = 'https://s.weibo.com/top/summary?cate=realtimehot'
-    response = urlopen(hot_url,timeout=10)
+    response = urlopen(hot_url,timeout=30)
     soup = BeautifulSoup(response, 'lxml')
     hot_list = []
     print('key_word,number, read, discuss')
+    index = 1
     for i in soup.find_all(name='tr'):
         text = i.find('td','td-02')
         if text is not None:
             key_word = text.a.contents[0]
-            link = text.a.attrs['href']
+            # link = text.a.attrs['href']
             if text.span is not None:
                 number = text.span.string
                 read, discuss = get_read_discuss(key_word)
-                print(year, month, day, hour, minute,key_word, number, read, discuss)
-                hot_list.append([year, month, day, hour, minute, key_word, number, read, discuss])
+                for try_count in range(3):  # retry to get read and discuss
+                    if read != -1:
+                        break
+                    else:
+                        print('retry:', try_count + 1)
+                        read, discuss = get_read_discuss(key_word)
+                        time.sleep(2)
+                time.sleep(2)
+                print(index, year, month, day, hour, minute,key_word, number, read, discuss)
+                hot_list.append([index, year, month, day, hour, minute, key_word, number, read, discuss])
+                index += 1
     return hot_list
 
 
@@ -70,10 +82,11 @@ def write_csv(hot_list):
         for row in hot_list:
             writer.writerow(row)
 
+
 while True:
     year, month, day, hour, minute = get_time()
     if minute in ['00', '10', '20', '30', '40', '50']:
-    #if True:
+    # if True:
         try:
             hot_list = get_hot()
             print("Writing file")
