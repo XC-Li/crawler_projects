a573cb791813d16d19f7be4a55670683018865e4
Xiaochi (George) Li <343928729@qq.com>
2018-12-29 01:13:48 -0500

    add document

--- weibo_hot_topic/readme.md
+++ weibo_hot_topic/readme.md
@@ -1,5 +1,25 @@
-# Crawler for hot topics on Sina Weibo
-A Christmas Present Project for Miss C.
+# Crawler and Analysis for hot topics on Weibo
+*A Project inspired by Miss C.*
 
-Designed by: Xiaochi (George) Li  
+Designed By: Xiaochi (George) Li  
 Dec.2018
+
+## Crawler Part
+This program([*weibo_hot_crawler.py*](./weibo_hot_crawler.py)) fetches the hot words and heat index from Hot Weibo(热搜榜） (https://s.weibo.com/top/summary?cate=realtimehot),
+read count and discussion count from Weibo Search（关键词搜索）(https://s.weibo.com/weibo?q) every 10 minutes
+It then stores the information into a CSV file.
+
+CSV contains: 10 columns(index, year, month, day, hour, minute, key_word, number, read, discuss)
+
+|Column name|Explanation|
+|----|----|
+|index（序号）| the current rank of the key_word|
+|year|-|
+|month|-|
+|day|-|
+|hour|-|
+|minute|-|
+|key_word（关键词）| the hot word (in chinese, encoding: utf-8)|
+|number（热搜指数）| the heat index|
+|read(阅读）| the read count|
+|discussion（讨论）| the discussion count|
--- weibo_hot_topic/weibo_hot_crawler.py
+++ weibo_hot_topic/weibo_hot_crawler.py
@@ -1,3 +1,21 @@
+"""
+Web Crawler for Weibo (Chinese version of twitter) Hot Words(微博热搜）
+A Project inspired by Miss C
+Designed By: Xiaochi (George) Li
+
+This program fetches the hot words and heat index from Hot Weibo(热搜榜） (https://s.weibo.com/top/summary?cate=realtimehot),
+read count and discussion count from Weibo Search（关键词搜索）(https://s.weibo.com/weibo?q) every 10 minutes
+It then stores the information into a CSV file.
+
+Defined in get_hot()
+CSV contains: 10 columns(index, year, month, day, hour, minute, key_word, number, read, discuss)
+index（序号）: the current rank of the key_word
+key_word（关键词）: the hot word (in chinese, encoding: utf-8)
+number（热搜指数）: the heat index
+read(阅读）: the read count
+discussion（讨论）: the discussion count
+"""
+
 from urllib.request import urlopen
 from urllib.parse import quote
 from bs4 import BeautifulSoup
@@ -8,6 +26,7 @@ import os
 
 
 def transform_number(s):
+    """Transfer number with chinese unit into real number"""
     if s[-1] == '亿':
         return int(float(s[:-1])*1e8)
     elif s[-1] == '万':
@@ -17,6 +36,13 @@ def transform_number(s):
 
 
 def get_read_discuss(keyword):
+    """get read count and discussion count
+    :parameter
+        keyword: keyword in chinese, encoding: utf-8
+    :return
+        read, discuss float
+    :except
+        return -1, -1"""
     link = quote('#'+keyword+'#')
     url = 'https://s.weibo.com/weibo?q='+link
     try:
@@ -32,6 +58,7 @@ def get_read_discuss(keyword):
 
 
 def get_time():
+    """return date and time in tuple"""
     if time.localtime(time.time()).tm_isdst == 0:
         dt = str(datetime.now() + timedelta(hours=13))
     else:
@@ -46,6 +73,12 @@ def get_time():
 
 
 def get_hot():
+    """get hot words
+    :return
+        hot_list contain 10 columns
+        (index:int, year:str, month:str, day:str, hour:str, minute:str,
+        key_word:str, number:int, read:float, discuss:float)
+    """
     year, month, day, hour, minute = get_time()
     hot_url = 'https://s.weibo.com/top/summary?cate=realtimehot'
     response = urlopen(hot_url,timeout=30)
@@ -54,20 +87,20 @@ def get_hot():
     print('key_word,number, read, discuss')
     index = 1
     for i in soup.find_all(name='tr'):
-        text = i.find('td','td-02')
+        text = i.find('td', 'td-02')
         if text is not None:
             key_word = text.a.contents[0]
             # link = text.a.attrs['href']
             if text.span is not None:
                 number = text.span.string
                 read, discuss = get_read_discuss(key_word)
-                for try_count in range(3):  # retry to get read and discuss
+                for try_count in range(5):  # More Stable: retry to get read and discuss
                     if read != -1:
                         break
                     else:
                         print('retry:', try_count + 1)
                         read, discuss = get_read_discuss(key_word)
-                        time.sleep(2)
+                        time.sleep(try_count + 2)
                 time.sleep(2)
                 print(index, year, month, day, hour, minute,key_word, number, read, discuss)
                 hot_list.append([index, year, month, day, hour, minute, key_word, number, read, discuss])
@@ -76,6 +109,9 @@ def get_hot():
 
 
 def write_csv(hot_list):
+    """write to csv
+    :parameter
+        hot_list, 2d list"""
     year, month, _, _, _ = get_time()
     with open(str(year)+'-'+str(month)+'.csv', 'a') as csvFile:
         writer = csv.writer(csvFile, lineterminator='\n')
@@ -83,10 +119,11 @@ def write_csv(hot_list):
             writer.writerow(row)
 
 
+"""Main Function"""
 while True:
     year, month, day, hour, minute = get_time()
     if minute in ['00', '10', '20', '30', '40', '50']:
-    # if True:
+    # if True:  # For Debug
         try:
             hot_list = get_hot()
             print("Writing file")
@@ -95,11 +132,11 @@ while True:
             print("End")
             break
         except:
-            print('some error',year, month, day, hour, minute)
+            print('some error', year, month, day, hour, minute)
             time.sleep(10)
 
     else:
-        print('Waiting',year, month, day, hour, minute)
+        print('Waiting', year, month, day, hour, minute)
         time.sleep(10)
         os.system('clear')
 
